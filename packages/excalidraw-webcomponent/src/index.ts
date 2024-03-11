75c9866defe5bba5eaf058cbd6fe58ca8bf6142c
siddharthasarma <sidduinevitable@gmail.com>
2024-03-11 08:12:08 +0530

    convert excalidraw component to webcomponent

--- packages/excalidraw-webcomponent/src/index.ts
+++ packages/excalidraw-webcomponent/src/index.ts
@@ -1,8 +1,12 @@
 import r2wc from '@r2wc/react-to-web-component'
 import {Header} from './Header'
+import {Excalidraw} from '@excalidraw/excalidraw'
 
 customElements.define(
   "rwc-header", r2wc(Header, {
     props: { text: "string" },
-  })
+  }),
+);
+customElements.define(
+  "rwc-excalidraw", r2wc(Excalidraw),
 );
